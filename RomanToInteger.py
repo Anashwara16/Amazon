03d86e62f832376e73cddd61eaac70236a03dadd
Anashwara <73360159+Anashwara16@users.noreply.github.com>
2022-05-29 11:55:18 -0700

    Easy

---
+++ RomanToInteger.py
@@ -0,0 +1,34 @@
+
+# 13. Roman to Integer
+
+
+class Solution:
+    def romanToInt(self, s: str) -> int:
+
+        roman = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000,
+        }
+
+        total = 0
+
+        for i in range(len(s)):
+
+            if (i+1) < len(s) and roman[s[i]] < roman[s[i+1]]:
+                total -= roman[s[i]]
+
+            else:
+                total += roman[s[i]]
+
+        return total
+
+
+if __name__ == "__main__":
+    s = "MCMXCIV"
+    objectNums = Solution()
+    print(objectNums.romanToInt(s))
