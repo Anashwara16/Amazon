86cceb034b7828c45b21b4a67581c96232f2c019
Anashwara <73360159+Anashwara16@users.noreply.github.com>
2022-07-07 15:08:16 -0700

    Add files via upload

--- longestConsecutiveSequence.py
+++ longestConsecutiveSequence.py
@@ -1,26 +1,26 @@
+
 # 128. Longest Consecutive Sequence
 
+
 class Solution:
-    def longestConsecutive(self, nums: list[int]) -> int:
-        longestStreak = 0
-        numSet = set(nums)
+    def productExceptSelf(self, nums: list[int]) -> list[int]:
 
-        for num in numSet:
+        array = [0]*len(nums)
+        array[0] = 1
 
-            if num-1 not in numSet:
-                currentNum = num
-                currentStreak = 1
+        for i in range(1, len(nums)):
+            array[i] = array[i-1] * nums[i-1]
 
-                while currentNum+1 in numSet:
-                    currentNum += 1
-                    currentStreak += 1
+        postFix = 1
 
-                longestStreak = max(longestStreak, currentStreak)
+        for i in reversed(range(len(nums))):
+            array[i] = postFix * array[i]
+            postFix *= nums[i]
 
-        return longestStreak
+        return array
 
 
 if __name__ == "__main__":
-    nums = [100, 4, 200, 1, 3, 2]
+    nums = [1, 2, 3, 4]
     objectNums = Solution()
-    print(objectNums.longestConsecutive(nums))
+    print(objectNums.productExceptSelf(nums))
