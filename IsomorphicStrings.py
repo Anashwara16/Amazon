23aaded659950c2c6f34c4ba64cfa80b9ba13958
Anashwara <73360159+Anashwara16@users.noreply.github.com>
2022-07-07 15:06:48 -0700

    Easy

---
+++ IsomorphicStrings.py
@@ -0,0 +1,26 @@
+
+# 205. Isomorphic Strings
+
+
+class Solution:
+
+    def transformString(self, s: str) -> str:
+        index_mapping = {}
+        new_str = []
+
+        for i, c in enumerate(s):
+            if c not in index_mapping:
+                index_mapping[c] = i
+            new_str.append(str(index_mapping[c]))
+
+        return " ".join(new_str)
+
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        return self.transformString(s) == self.transformString(t)
+
+
+if __name__ == "__main__":
+    s = "egg"
+    t = "add"
+    objectNums = Solution()
+    print(objectNums.isIsomorphic(s, t))
